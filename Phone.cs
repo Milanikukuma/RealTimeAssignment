0010732950e6f0d15a33163375fec1d4859c0794
Milanikukuma <kukumamilani62@gmail.com>
2023-12-12 07:27:44 +0200

    first commit

---
+++ Phone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorld
+{
+    internal class Phone
+    {
+        private string brand;
+        private string model;
+        private int storage;
+
+        public Phone(string brand, string model, int storage)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.storage = storage;
+        }
+
+        public void MakeCall(string contact)
+        {
+            Console.WriteLine($"Calling {contact}...");
+        }
+
+        public void SendText(string contact, string message)
+        {
+            Console.WriteLine($"Sending text to {contact}: {message}");
+        }
+
+        public void CheckStorage() // Public method to access storage info
+        {
+            Console.WriteLine($"Available storage on {brand} {model}: {storage}GB");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace RealWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Phone myPhone = new Phone("Apple", "iPhone 13", 256);
+
+        
+            myPhone.MakeCall("John");
+
+            
+            myPhone.SendText("Sarah", "Hello!");
+
+          
+            myPhone.CheckStorage();
+        }
+    }
+}
